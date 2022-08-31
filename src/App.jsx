945ebe458a2201fdf79609804e26bee8debde27f
Wasim Raja <wasim7raja10@gmail.com>
2022-08-31 22:23:03 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import twitterLogo from "./icons/002-twitter.png"
+import githubLogo from "./icons/003-github.png"
+import linkedinLogo from "./icons/001-linkedin.png"
 
 function App() {
   const [birthday, setBirthday] = useState("");
@@ -67,7 +70,36 @@ function App() {
         {isLucky && <h2>{isLucky}</h2>}
       </main>
       <footer>
-        <p>footer</p>
+        <h6>created by <a href="https://wasimraja.me/portfolio">Wasim Raja</a></h6>
+        <ol>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/wasim-raja-31857014b/"
+              rel="noreferrer"
+            >
+              <img src={linkedinLogo} alt="linkedin logo" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://twitter.com/wasim7raja10"
+              rel="noreferrer"
+            >
+              <img src={twitterLogo} alt="twitter logo" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://github.com/wasim7raja10"
+              rel="noreferrer"
+            >
+              <img src={githubLogo} alt="github logo" />
+            </a>
+          </li>
+        </ol>
       </footer>
     </>
   );
